9dd1fa4babe6d8acbfa669a086c8bff81227d948
Yassine Qoraiche <qoraicheofficiel@hotmail.com>
2022-02-15 01:15:51 +0100

    add task1 validate username function

--- task1.php
+++ task1.php
@@ -1,2 +1,22 @@
 <?php
-// Init GL
+
+
+/**
+ * Validation function
+ * the function accepts a username and returns true if it passes the implemented criteria or false if it does not pass.
+ *
+ * @param string $username
+ * @return bool
+ */
+function validateUsername(string $username): bool
+{
+    return preg_match('/^(?=[a-zA-Z0-9_]{4,}$)(?!.*[_]{2})[^_].*[^_]$/i', $username);
+}
+
+/**
+ * var_dump(validateUsername('yassine')); //bool(true)
+ * var_dump(validateUsername('yas sine')); // bool(false)
+ * var_dump(validateUsername('yas_sine')); // bool(true)
+ * var_dump(validateUsername('yas_sine_')); // bool(false)
+ * var_dump(validateUsername('yas')); // bool(false)
+ */
