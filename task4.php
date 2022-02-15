4c11e98dce81bd4f9dd5e7c2e76d81538cf0d83d
Yassine Qoraiche <qoraicheofficiel@hotmail.com>
2022-02-15 04:19:20 +0100

    Keep it simple

--- task4.php
+++ task4.php
@@ -1,50 +1,12 @@
 <?php
 
 /**
- *
  * @param array $dates
  * @return array
  */
-function transformDateFormat(array $dates)
+function transformDateFormat(array $dates): array
 {
-    $validDates = [];
-
-    foreach ($dates as $date) {
-        $delimiter = strpos($date, '/') !== false ? '/' : '-';
-        $dateData = explode($delimiter, $date);
-
-        $firstDateElementLength = $dateData[0];
-        $secondDateElementLength = $dateData[0];
-        $thirdDateElementLength = $dateData[0];
-
-        if (isValidYear($firstDateElementLength) && isValidMonth($secondDateElementLength) && isValidDay($thirdDateElementLength)) {
-            $validDates[] = $date;
-        }
-
-        if (isValidDay($firstDateElementLength) && isValidMonth($secondDateElementLength) && isValidYear($thirdDateElementLength)) {
-            $validDates[] = $date;
-        }
-
-        if (isValidMonth($firstDateElementLength) && isValidMonth($secondDateElementLength) && isValidYear($thirdDateElementLength)) {
-            $validDates[] = $date;
-        }
-    }
+    return preg_grep('/^(?:\d{4}\/\d{2}\/\d{2}|\d{2}\/\d{2}\/\d{4}|\d{2}-\d{2}-\d{4})$/i', $dates);
 }
 
-transformDateFormat(['2020/07/12', '26/07/2021', '06-12-1994']);
-
-
-function isValidYear($year)
-{
-    return $year > 1000 && $year < 2100;
-}
-
-function isValidMonth($month)
-{
-    return $month > 0 && $month < 12;
-}
-
-function isValidDay($day)
-{
-    return $day > 0 && $day < 31;
-}
+transformDateFormat(['2020/07/12', '26/07/2021', '06-12-1994', '897-987-879']); // ['2020/07/12', '26/07/2021', '06-12-1994']
